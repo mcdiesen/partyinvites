b4dfade51ec4bef9a96abcb5bb5b704bbcf5b8d5
Charles McLean <mcdiesen@gmail.com>
2023-05-28 20:30:59 -0400

    main file modified

--- main.go
+++ main.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"html/template"
+)
 
 type Rsvp struct {
 	Name, Email, Phone string
@@ -9,8 +12,26 @@ type Rsvp struct {
 
 // responses new slice of Rsvp struct
 var responses = make([]*Rsvp, 0, 10)
+var templates = make(map[string]*template.Template, 3)
 
+func loadTemplates() {
+	//TODO - load templates here
+	templateNames := [5]string{"welcome", "form", "thanks", "sorry", "list"}
+
+	for index, name := range templateNames {
+		t, err := template.ParseFiles("layout.html", name+".html")
+		if err == nil {
+			templates[name] = t
+			fmt.Println("Loaded template", index, name)
+		} else {
+			panic(err)
+		}
+	}
+}
 func main() {
-	// ...
+	// 	...
 	fmt.Println("TODO: add some features!")
+	fmt.Println("Welcome to my party!")
+
+	loadTemplates()
 }
